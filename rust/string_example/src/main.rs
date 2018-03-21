5ebe013b5337ab30c39493bf6179e6d820871245
frozar <fabien.rozar@gmail.com>
2018-03-21 18:28:50 +0100

    [RUST] Add string manipulation project: strings are not so simple.

---
+++ rust/string_example/src/main.rs
@@ -0,0 +1,74 @@
+fn main() {
+    example_1();
+    example_2();
+    example_3();
+    example_4();
+}
+
+fn example_1() {
+    let mut s = String::new();
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+    let s = String::from("initial contents");
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+}
+
+fn example_2() {
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(&s2);
+    println!("s2 is {}", s2);
+    
+    let mut s = String::from("lo");
+    s.push('l');
+}
+
+fn example_3() {
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // Note that s1 has been moved here and can no longer be used
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+}
+
+fn example_4() {
+    let hello = "Здравствуйте";
+
+    let s = &hello[0..4];
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+}
