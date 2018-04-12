9ff71a90fe27c6b84eda450e3ec5994cebb0b2f7
frozar <fabien.rozar@gmail.com>
2018-04-13 00:14:24 +0200

    [RUST] Complete the project about cargo workspace.

--- rust/ch14_03_add/adder/src/main.rs
+++ rust/ch14_03_add/adder/src/main.rs
@@ -1,3 +1,14 @@
+extern crate add_one;
+
+// As the 'rand' crate is not specified as a dependencies for the
+// current crate, "adder", one get a compiling error if one try to
+// use it, even if it is a dependencies for the "add-one" crate
+// extern crate rand;
+
 fn main() {
-    println!("Hello, world!");
+    let num = 10;
+    println!("Hello, world! {} plus one is {}!", num, add_one::add_one(num));
 }
+
+// Run a given crate, for example "adder":
+// $ cargo run -p adder
