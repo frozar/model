a9b6b00bcc44cc86807f369908b5ffa3b2cf9e52
frozar <fabien.rozar@gmail.com>
2019-09-27 13:26:47 +0200

    [CLJS] tutorial-05

---
+++ cljs/modern-cljs/tutorial-05/src/cljs/modern_cljs/login.cljs
@@ -0,0 +1,26 @@
+(ns modern-cljs.login
+  (:require [domina.core :refer [by-id value set-value!]]))
+
+;; define the function to be attached to form submission event
+(defn validate-form []
+  ;; get email and password element from their ids in the HTML form
+  (if (and (> (count (value (by-id "email"))) 0)
+           (> (count (value (by-id "password"))) 0))
+    true
+    (do (js/alert "Please, complete the form!")
+          false)))
+
+;; define the function to attach validate-form to onsubmit event of
+;; the form
+(defn init []
+  ;; verify that js/document exists and that it has a getElementById
+  ;; property
+  (if (and js/document
+           (.-getElementById js/document))
+    ;; get loginForm by element id and set its onsubmit property to
+    ;; our validate-form function
+    (let [login-form (.getElementById js/document "loginForm")]
+      (set! (.-onsubmit login-form) validate-form))))
+
+;; initialize the HTML page in unobtrusive way
+(set! (.-onload js/window) init)
