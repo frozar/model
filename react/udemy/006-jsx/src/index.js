6d8e61054be1018fc88a5d3a49e8e4b43b706065
frozar <fabien.rozar@gmail.com>
2020-09-29 16:57:53 +0200

    React: First example from udemy course

---
+++ react/udemy/006-jsx/src/index.js
@@ -0,0 +1,29 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+// A react component can be a class or a function.
+// Here is an example of a function component.
+const App = function () {
+  const buttonText = 'Click me';
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+    </div>
+    );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
