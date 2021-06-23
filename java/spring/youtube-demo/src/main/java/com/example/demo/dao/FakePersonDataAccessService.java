86e17cc72c939f78687818510cf70a796ded2631
frozar <fabien.rozar@gmail.com>
2021-06-23 22:28:41 +0400

    Use spring boot to setup the GET route api/v1/person: allow to retrieve 1 people from DB.

--- java/spring/youtube-demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ java/spring/youtube-demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -2,6 +2,7 @@ package com.example.demo.dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
@@ -28,6 +29,25 @@ public class FakePersonDataAccessService implements PersonDao {
   @Override
   public List<Person> selectAllPeople() {
     return DB;
+  }
+
+  @Override
+  public Optional<Person> selectPersonById(UUID id) {
+    return DB.stream()
+        .filter(person -> person.getId().equals(id))
+        .findFirst();
+  }
+
+  @Override
+  public int deletePersonById(UUID id) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int updatePersonById(UUID id, Person person) {
+    // TODO Auto-generated method stub
+    return 0;
   }
 
 }
